95cf8c4caa3da070cf04bdacec2143a9635ed471
Snehal Dutta <snehaldutta30@gmail.com>
2026-03-15 00:54:17 +0530

    climbingstairs

---
+++ Python_Prog/ClimbingStairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n):
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        prev2 = 1  
+        prev1 = 2 
+
+        for _ in range(3, n + 1):
+            current = prev1 + prev2
+            prev2 = prev1
+            prev1 = current
+
+        return prev1
